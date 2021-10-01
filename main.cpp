b44a29d9e56997435e7aca32ee9743d0213aa684
tbl4h <45106120+tbl4h@users.noreply.github.com>
2021-10-01 22:57:45 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,116 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+#include <curl/curl.h>
+#include <vector>
+
+struct MemoryStruct
+{
+  char *memory;
+  size_t size;
+};
+std::vector<std::string> vURLaddress = {
+    "http://www.example.com",
+    "https://justjoin.it",
+    "https://www.wp.pl/",
+    "https://github.com/"
+};
+std::vector<int> handshakeSize(vURLaddress.size());
+
+static int callIteration = 0;
+static int addressCurrentIteration = 0;
+static int urlChange = -1;
+static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp)
+{
+  size_t realsize = size * nmemb;
+  struct MemoryStruct *mem = (struct MemoryStruct *)userp;
+
+  char *ptr = (char *)realloc(mem->memory, mem->size + realsize + 1);
+  if (!ptr)
+  {
+    /* out of memory! */
+    printf("not enough memory (realloc returned NULL)\n");
+    return 0;
+  }
+
+  mem->memory = ptr;
+  memcpy(&(mem->memory[mem->size]), contents, realsize);
+  mem->size += realsize;
+  mem->memory[mem->size] = 0;
+  std::cout << callIteration << " real size: " << realsize << std::endl;
+
+  if (urlChange != addressCurrentIteration)
+  {
+    std::cout << "Network package size for current url address: " << vURLaddress[addressCurrentIteration] << std::endl;
+    handshakeSize[addressCurrentIteration] = realsize;
+    std::cout << addressCurrentIteration << " handshake size for different domain addres: " << handshakeSize[addressCurrentIteration] << std::endl;
+    urlChange += 1;
+    // std::cout << "size argument " << size << std::endl;
+    std::cout << "nmemb " << nmemb << std::endl;
+  }
+  callIteration++;
+  return realsize;
+}
+
+int main(void)
+{
+  CURL *curl_handle;
+  CURLcode res;
+
+  struct MemoryStruct chunk;
+
+  chunk.memory = (char *)malloc(1); /* will be grown as needed by the realloc above */
+  chunk.size = 0;                   /* no data at this point */
+
+  curl_global_init(CURL_GLOBAL_ALL);
+
+  /* init the curl session */
+  curl_handle = curl_easy_init();
+
+  /* send all data to this function  */
+  curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+
+  /* we pass our 'chunk' struct to the callback function */
+  curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&chunk);
+
+  /* some servers don't like requests that are made without a user-agent
+     field, so we provide one */
+  // curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+
+  for (addressCurrentIteration = 0; addressCurrentIteration < vURLaddress.size(); addressCurrentIteration++)
+  {
+    /* specify URL to get */
+    curl_easy_setopt(curl_handle, CURLOPT_URL, "https://www.wp.pl/");
+
+    /* get it! */
+    res = curl_easy_perform(curl_handle);
+
+    /* check for errors */
+    if (res != CURLE_OK)
+    {
+      fprintf(stderr, "curl_easy_perform() failed: %s\n",
+              curl_easy_strerror(res));
+    }
+    else
+    {
+      /*
+     * Now, our chunk.memory points to a memory block that is chunk.size
+     * bytes big and contains the remote file.
+     *
+     * Do something nice with it!
+     */
+
+      printf("%lu bytes retrieved\n", (unsigned long)chunk.size);
+    }
+  }
+  /* cleanup curl stuff */
+  curl_easy_cleanup(curl_handle);
+
+  free(chunk.memory);
+
+  /* we're done with libcurl, so clean it up */
+  curl_global_cleanup();
+
+  return 0;
+}
